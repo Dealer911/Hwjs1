c53acea43ce34e65c71c938363d723c63757b06c
Dealer911 <opaganamyo@gmail.com>
2022-04-14 18:02:16 +0300

    Initial commit

---
+++ main.js
@@ -0,0 +1,105 @@
+let b;
+b = 'hello';
+console.log(b);
+
+let d;
+d = 'owu';
+console.log(d);
+
+let v;
+v = 'com';
+console.log(v);
+
+let g;
+g = 'ua';
+console.log(g);
+
+let a;
+a = '1';
+console.log(a);
+
+let n;
+n = '10';
+console.log(n);
+
+let m;
+m = '-999';
+console.log(m);
+
+let w;
+w = '123';
+console.log(w);
+
+let s;
+s = '3.14';
+console.log(s);
+
+let q;
+q = '2.7';
+console.log(q);
+
+let r;
+r = '16';
+console.log(r);
+
+let e;
+e = 5 < 10;
+console.log(e);
+
+let y;
+y = 10 < 5;
+console.log(y);
+
+let book = {
+    name: "Harry Poter",
+    pages: "300",
+    genre: "Fantazy",
+}
+
+console.log(book.name);
+console.log(book.pages);
+console.log(book.genre);
+
+let books = {
+    name: "The Ungly Duckling",
+    pages: "50",
+    genre: "miniature tales",
+    authors: [
+        "Hans Christian Andersen",
+    ]
+}
+
+console.log(books.name);
+console.log(books.pages);
+console.log(books.genre);
+
+const firstName = "Бойко",
+      middleName = "Ярослав",
+      lastName = "Іванович";
+
+const person = firstName + middleName + lastName;
+console.log(person)
+
+let name  = prompt("А як тебе звати?", "Ярослав"),
+    fathers = prompt("А по батькові як?", "Іванович"),
+    age = prompt("А років скільки?", "18");
+
+console.log(name, fathers, age);
+
+let first = "100",
+    two = 100,
+    c = true;
+console.log(typeof two, first, c);
+
+let boooks = [
+    {name: "Harry Potter", pages:"350", genre:"Fantasy", authors:"Joan Rowling" },
+    {name:"The Ungly Duckling", pages:"50", genre:"miniature tales", authors:"Hans Christian Andersen"},
+    {name:"The Lion King", pages:"100", genre:"musical drama adventure family", authors:"Don Khan"},
+    {name:"Adventures of strangers", pages:"150", genre: "Artistic fiction", authors:"Nikolai Nikolaevich Nosov"}
+];
+console.log(boooks[0]);
+console.log(boooks[1]);
+console.log(boooks[2]);
+console.log(boooks[3]);
+
+
